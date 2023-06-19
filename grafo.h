74788f12daf6e791ec0150702dd48d3fdaf0133d
Lucas Henrique Longato Rodrigues <81321261+LucasLongato@users.noreply.github.com>
2023-06-19 20:51:39 -0300

    Update grafo.h

--- grafo.h
+++ grafo.h
@@ -4,93 +4,34 @@
 #include <stdbool.h>
 
 // Structs para o grafo
-struct artista {
-  int id;
-  char *nome;
-  char **filmes;
-  int num_filmes;
-}artistas;
+typedef struct {
+    char* titleId;
+    char* titleType;
+    char* primaryTitle;
+    char* originalTitle;
+    int isAdult;
+    int startYear;
+    int endYear;
+    int runtimeMinutes;
+    char* genres;
+    int* artists; // Array of artist IDs associated with the movie
+    int numArtists; // Number of artists associated with the movie
+} filmes;
 
-struct filme {
-  int id;
-  char *nome;
-  int num_artistas;
-};
+typedef struct {
+    int ID;
+    char* nome;
+    char** movies;
+    int numMovies;
+} Artistas;
 
-struct grafo {
-  struct filme **vertices;
-  int num_vertices;
-}grafo;
+typedef struct {
+    int* adjMatrix;
+    int numVertices;
+} Grafo;
 
-// funçao para ler o arquivo e criar os vetores de artistas e filmes
-void ler_artistas(FILE *arquivo, struct artista **artistas) {
-  int i, num_artistas;
-  char linha[1024];
+void addEdge(Grafo* grafo, filme* filme int movieIndex1, int movieIndex2);
+grafh criargrafo(filme* filmes, int numfilmes) ;
+void grafoprint(grafh grafo, filme* filmes);
 
-  fscanf(arquivo, "%d", &num_artistas);
-  *artistas = malloc(sizeof(struct artista) * num_artistas);
 
-  for (i = 0; i < num_artistas; i++) {
-    fgets(linha, sizeof(linha), arquivo);
-    sscanf(linha, "%d %[^\n]", &(*artistas)[i].id, (*artistas)[i].nome);
-    (*artistas)[i].filmes = NULL;
-    (*artistas)[i].num_filmes = 0;
-  }
-}
-
-void ler_filmes(FILE *arquivo, struct filme **filmes) {
-  int i, num_filmes;
-  char linha[1024];
-
-  fscanf(arquivo, "%d", &num_filmes);
-  *filmes = malloc(sizeof(struct filme) * num_filmes);
-
-  for (i = 0; i < num_filmes; i++) {
-    fgets(linha, sizeof(linha), arquivo);
-    sscanf(linha, "%d %[^\n]", &(*filmes)[i].id, (*filmes)[i].nome);
-    (*filmes)[i].num_artistas = 0;
-  }
-}
-
-// funçao para adicionar um filme ao artista
-void adicionar_filme_ao_artista(struct artista *artista, struct filme *filme) {
-  artista->filmes = realloc(artista->filmes, sizeof(char *) * (artista->num_filmes + 1));
-  artista->filmes[artista->num_filmes++] = filme->nome;
-}
-
-// funçao para adicionar um artista ao filme
-void adicionar_artista_ao_filme(struct filme *filme, struct artista *artista) {
-  filme->num_artistas++;
-}
-
-// funçao para construir o grafo
-void construir_grafo(struct artista **artistas, struct filme **filmes, struct grafo **grafo) {
-  int i, j;
-
-  *grafo = malloc(sizeof(struct grafo));
-  (*grafo)->vertices = malloc(sizeof(struct filme *) * (*filmes)->num_filmes);
-  (*grafo)->num_vertices = (*filmes)->num_filmes;
-
-  for (i = 0; i < (*filmes)->num_filmes; i++) {
-    (*grafo)->vertices[i] = filmes[i];
-    for (j = 0; j < (*artistas)->num_filmes; j++) {
-      if (strstr(filmes[i].nome, artistas[j].nome) != NULL) {
-        adicionar_filme_ao_artista(artistas[j], filmes[i]);
-        adicionar_artista_ao_filme(filmes[i], artistas[j]);
-      }
-    }
-  }
-}
-
-// funçao para imprimir o grafo
-void imprimir_grafo_dot(struct grafo *grafo) {
-  int i, j;
-
-  printf("graph {\n");
-  for (i = 0; i < grafo->num_vertices; i++) {
-    for (j = 0; j < grafo->vertices[i]->num_artistas; j++) {
-      printf("\t%s -- %s;\n", grafo->vertices[i]->nome, artistas[j].nome);
-    }
-  }
-  printf("}\n");
-}
